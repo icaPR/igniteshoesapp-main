a38574af1af2939ae0cbda74cc030c26a1abfc61
Icaro Ramos <polosnramos@hotmail.com>
2024-04-05 13:27:05 -0300

    feat: Add Notification to application routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,48 @@
-import { useTheme } from 'native-base';
-import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
-
-import { AppRoutes } from './app.routes';
+import { useTheme } from "native-base";
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
+import {
+  NotificationWillDisplayEvent,
+  OSNotification,
+  OneSignal,
+} from "react-native-onesignal";
+import { AppRoutes } from "./app.routes";
+import { useEffect, useState } from "react";
+import { Notification } from "../components/Notification";
 
 export function Routes() {
   const { colors } = useTheme();
+  const [notification, setNotification] = useState<OSNotification>();
 
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
 
+  useEffect(() => {
+    const handleNotificarion = (event: NotificationWillDisplayEvent): void => {
+      event.preventDefault();
+      const response = event.getNotification();
+      setNotification(response);
+    };
+    OneSignal.Notifications.addEventListener(
+      "foregroundWillDisplay",
+      handleNotificarion
+    );
+    return () =>
+      OneSignal.Notifications.removeEventListener(
+        "foregroundWillDisplay",
+        handleNotificarion
+      );
+  }, []);
   return (
     <NavigationContainer theme={theme}>
       <AppRoutes />
+      {notification && (
+        <Notification
+          data={notification}
+          onClose={() => {
+            setNotification(undefined);
+          }}
+        />
+      )}
     </NavigationContainer>
   );
 }
